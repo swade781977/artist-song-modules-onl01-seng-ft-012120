b736105dc7abdfeb42083274a9e6d1caf03ba254
Chris Wade <swade781977@yahoo.com>
2020-02-08 22:07:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,18 +14,12 @@ class Song
     @@songs << self
   end
 
-  #def self.find_by_name(name)
-    #@@songs.detect{|a| a.name == name}
-  #end
-
   def self.all
     @@songs
   end
-
 
   def artist=(artist)
     @artist = artist
   end
 
-  
 end
