b5d77e23f96dc67f7b8db3e8ac06f57a555267e0
Chris Wade <swade781977@yahoo.com>
2020-02-08 22:07:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,13 +15,10 @@ class Artist
     @songs = []
   end
 
-  
-
   def self.all
     @@artists
   end
-
-
+  
   def add_song(song)
     @songs << song
     song.artist = self
@@ -31,5 +28,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  
 end
